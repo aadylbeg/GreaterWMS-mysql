0fd97b337c2ad014ed2b3622738db5fdcaf4451c
Adil Bek <hudaybergenowadilbek@gmail.com>
2022-09-03 14:54:21 -0700

    mysql

--- greaterwms/settings.py
+++ greaterwms/settings.py
@@ -104,12 +104,27 @@ WSGI_APPLICATION = 'greaterwms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 # update
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# -----------------------------------HEEEEEEEEEEEEEEREEEEEEEEEEEEEEEEEEEEE---------------------------------------------
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'my_database',  
+        'USER': 'root',  
+        'PASSWORD': 'Password123#@!',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
+} 
 
 
 # Password validation
